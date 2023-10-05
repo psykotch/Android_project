0b47b5a88d425de04da9c4e2895db8e29f15c7fb
Mathilde <francois.machuron@epita.fr>
2023-10-05 08:52:19 +0200

    Fragments of all pages

---
+++ app/src/main/java/fr/epita/androidproject/fragments/PlayerDetailPage.kt
@@ -0,0 +1,26 @@
+package fr.epita.androidproject.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import fr.epita.androidproject.R
+
+class PlayerDetailPage : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_player_detail_page, container, false)
+    }
+}
